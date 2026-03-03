edfa3f2b9e1c240ab680c6a628a6c102c73d4f45
Archit Pandey <117747376+ArchitP9andey@users.noreply.github.com>
2026-03-03 23:17:37 +0530

    Add Streamlit data analytics web application
    This file implements a Streamlit web application for data analytics, allowing users to upload CSV or Excel files, view basic dataset information, and perform various data visualizations and group operations.

---
+++ myapp.py
@@ -0,0 +1,125 @@
+from email.quoprimime import header_encode
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(
+    page_title="DataWorkbench (A Data Analytics Web Portal)",
+    page_icon= '📊'
+)
+
+st.title(":rainbow[DataWorkbench]")
+st.subheader(':gray[A Smart Data Analytics Application]')
+st.subheader(':gray[Explore data with ease]',divider='rainbow')
+
+#fileuploader
+
+file = st.file_uploader("Drop your csv or excel file",type=["csv",'xlsx'])
+if (file!=None):
+    if(file.name.endswith('.csv')):
+        data = pd.read_csv(file)
+    else:
+        data = pd.read_excel(file)
+
+    st.dataframe(data)
+    st.info('File uploaded successfully',icon='✅')
+
+    st.subheader(':rainbow[Basic Information of the dataset]',divider='rainbow')
+    tab1,tab2,tab3,tab4 = st.tabs(['Summary','Top and Bottom Rows','Data Types','Columns'])
+
+    with tab1:
+        st.write(f'There are {data.shape[0]} rows in the dataset and {data.shape[1]} columns in the dataset')
+        st.subheader(':gray[Statistical Summary of the dataset]')
+        st.dataframe(data.describe())
+
+    with tab2:
+        st.subheader(':gray[Top Rows of the dataset]')
+        toprows = st.slider('Number of rows you want',1,data.shape[0],key='topslider')
+        st.dataframe(data.head(toprows))
+        st.subheader(':gray[Bottom Rows of the dataset]')
+        bottomrows = st.slider('Number of rows you want', 1, data.shape[0],key='bottomslider')
+        st.dataframe(data.tail(bottomrows))
+
+    with tab3:
+        st.subheader(':gray[Datatypes of Columns of the dataset]')
+        st.dataframe(data.dtypes)
+
+    with tab4:
+        st.subheader(':gray[Column Names in the dataset]')
+        st.write(list(data.columns))
+
+    st.subheader(':rainbow[Column Values To Count]',divider='rainbow')
+    with st.expander('Value Count'):
+        col1,col2 = st.columns(2)
+        with col1:
+          column = st.selectbox('Choose Column name',options=list(data.columns))
+        with col2:
+          toprows = st.number_input('Top rows',min_value=1,step=1)
+
+        count = st.button('Count')
+        if(count==True):
+            result = data[column].value_counts().reset_index().head(toprows)
+            st.dataframe(result)
+            st.subheader('Visualization',divider='gray')
+            fig = px.bar(data_frame=result,x=column,y='count',text='count',template='plotly_white')
+            st.plotly_chart(fig)
+            fig = px.line(data_frame=result,x=column,y='count',text='count',template='plotly_white')
+            st.plotly_chart(fig)
+            fig = px.pie(data_frame=result,names=column,values='count')
+            st.plotly_chart(fig)
+
+
+    st.subheader(':rainbow[Groupby: Simplify your data analysis]',divider='rainbow')
+    st.write('The Groupby lets you summarize data by specific categories anf groups')
+    with st.expander('Group by your columns'):
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            groupby_cols = st.multiselect('Choose your column to Groupby',options=list(data.columns))
+
+        with col2:
+            operation_col = st.selectbox('Choose your operation column',options=list(data.columns))
+
+        with col3:
+            operation = st.selectbox('Choose operation to perform',options=['sum', 'count','mean','min','max','median'])
+        if(groupby_cols):
+            result = data.groupby(groupby_cols).agg(
+                newcol=(operation_col, operation)
+            ).reset_index()
+
+            st.dataframe(result)
+
+            st.subheader(':rainbow[Data Visualization]',divider='rainbow')
+            graphs = st.selectbox('Choose you graphs',options=['line','bar','scatter','pie','sunburst'])
+            if(graphs=='line'):
+                x_axis = st.selectbox('Choose X axis',options=list(result.columns))
+                y_axis = st.selectbox('Choose Y axis', options=list(result.columns))
+                color = st.selectbox('Color Information',options = [None]+ list(result.columns))
+                fig = px.line(data_frame=result,x=x_axis,y=y_axis,color=color,markers='o')
+                st.plotly_chart(fig)
+            elif(graphs=='bar'):
+                x_axis = st.selectbox('Choose X axis', options=list(result.columns))
+                y_axis = st.selectbox('Choose Y axis', options=list(result.columns))
+                color = st.selectbox('Color Information', options=[None] + list(result.columns))
+                facet_col = st.selectbox('Column Information',options=[None] + list(result.columns))
+                fig = px.bar(data_frame=result, x=x_axis, y=y_axis, color=color,facet_col=facet_col,barmode='group')
+                st.plotly_chart(fig)
+            elif(graphs=='scatter'):
+                x_axis = st.selectbox('Choose X axis', options=list(result.columns))
+                y_axis = st.selectbox('Choose Y axis', options=list(result.columns))
+                color = st.selectbox('Color Information', options=[None] + list(result.columns))
+                size = st.selectbox('Size Column',options=[None] + list(result.columns))
+                fig = px.scatter(data_frame=result,x=x_axis,y=y_axis,color=color,size=size)
+                st.plotly_chart(fig)
+            elif (graphs == 'pie'):
+                values = st.selectbox('Choose Numerical Values',options=list(result.columns))
+                names = st.selectbox('Choose labels',options=list(result.columns))
+                fig = px.pie(data_frame=result,values=values,names=names)
+                st.plotly_chart(fig)
+            elif(graphs == 'sunburst'):
+                path = st.multiselect('Choose your path',options=list(result.columns))
+                fig  = px.sunburst(data_frame=result,path=path)
+                st.plotly_chart(fig)
+
+
+
